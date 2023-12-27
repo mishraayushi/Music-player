46641d3038c9c76c6d650e09218107becfe89884
Ayushi Mishra <134922095+mishraayushi@users.noreply.github.com>
2023-12-27 16:22:00 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+let songs= [
+    {
+        name:'song 1',
+        path: 'songs/song1.mp3',
+        artist: 'artist 1',
+        cover:'images/cover1.jpeg'
+    },
+    {
+        name:'song 2',
+        path: 'songs/song2.mp3',
+        artist: 'artist 2',
+        cover:'images/cover2.jpg'
+    },
+    {
+        name:'song 3',
+        path: 'songs/song3.mp3',
+        artist: 'artist 3',
+        cover:'images/cover3.jpeg'
+
+    },
+]
